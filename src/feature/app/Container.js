6910e5239ca53642ee0d94723b7f6d4e0184a98f
Truong <truong.nguyen@persol.co.jp>
2019-05-27 17:28:14 +0700

    share state between todo, apps feature

--- src/feature/app/Container.js
+++ src/feature/app/Container.js
@@ -1,20 +1,17 @@
 import React from "react";
 
-import { initialData } from "../../data";
-
 // components
 import { Todo } from "../../feature";
 
 // context
 import { AppProvider } from "./context/appContext";
 import { TodoProvider } from "../todo/context/todoContext";
-import { reducer as todoReducer } from "../todo/reducer/todoReducer";
 
 export function App() {
   return (
     <div className="App">
       <AppProvider>
-        <TodoProvider state={initialData} reducer={todoReducer}>
+        <TodoProvider>
           <h1>Build Todo Apps with React Hook and Context API</h1>
           <Todo />
         </TodoProvider>
--- src/feature/todo/context/TodoContext.js
+++ src/feature/todo/context/TodoContext.js
@@ -1,9 +1,14 @@
 import React, { useContext, createContext, useReducer } from "react";
 
+import { initialData } from "../../../data";
+
+// reducer
+import { reducer } from "../reducer/todoReducer";
+
 export const TodoContext = createContext();
 
-export const TodoProvider = ({ state, reducer, children }) => {
-  const [todoState, todoDispatcher] = useReducer(reducer, state);
+export const TodoProvider = ({ children }) => {
+  const [todoState, todoDispatcher] = useReducer(reducer, initialData);
 
   return (
     <TodoContext.Provider
